54ea1150f69b44407a6fdc7377d26692d38c9661
Dhiyaul-n <140733237+Dhiyaul-n@users.noreply.github.com>
2023-10-02 12:59:53 +0700

    Add files via upload

---
+++ profile_arrow_function.js
@@ -0,0 +1,6 @@
+const profile = (namadepan,namabelakang,email) => {
+  console.log("hai: " + namadepan + namabelakang)
+  console.log(email)
+}
+
+profile("dhiyaul","nawaz","222310256@smkn4-tsm.sch.id")
---
+++ profile_reg_function.js
@@ -0,0 +1,6 @@
+function profile(namadepan,namabelakang,email) {
+  console.log("Halo :" + namadepan +" " + namabelakang)
+  console.log("email: " + email)
+}
+
+profile("dhiyaul","nawaz","222310256@smkn4-tsm.sch.id")
